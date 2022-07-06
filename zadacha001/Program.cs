f07e0e02569aeda6c184f835f903c74b8b704b64
Ruslan Minzarskiy <Minzarsky@gmail.com>
2022-07-06 22:06:15 +0300

    решил задачу с рандомными числами

---
+++ zadacha001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу которая выводит случайное число из отрезка [10, 99] и показывает найбольшую
+// цифру числа.
+
+Random rnd = new Random();
+int sum = rnd.Next(10, 99);
+Console.WriteLine(sum);
+if (sum % 10 > sum / 10)
+{
+    Console.WriteLine(sum % 10);
+}
+else
+{
+    Console.WriteLine(sum / 10);
+}
